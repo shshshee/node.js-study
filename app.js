4953841602d7f5a93e1bea1dd7e0e6514c85b8e6
shshshee <sehhe22@naver.com>
2021-01-19 22:32:35 +0900

    add follow & delete follow

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
-sequelize.sync({ alter: true })
+sequelize.sync({ force: false })
     .then(() => {
         console.log('데이터베이스 연결 성공');
     })
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const {isLoggedIn} = require('./middlewares');
 const User = require('../models/user');
+const { Op } = require("sequelize");
 
 const router = express.Router();
 
+//팔로우하기 기능
 router.post('/:id/follow',isLoggedIn, async (req,res,next) => {
     try{
         const user = await User.findOne({where: {id: req.user.id}});
@@ -20,5 +22,26 @@ router.post('/:id/follow',isLoggedIn, async (req,res,next) => {
         next(error);
     }
 });
+
+//팔로우 취소 기능
+router.delete('/:id/delFollow', isLoggedIn, async(req,res,next) => {
+    try{
+        const user = await User.findOne({where: {id: req.user.id}});
+        const myId = parseInt(req.user.id,10);
+        if(user) {
+            await user.removeFollowing({
+                where:{
+                    followingId:req.params.id
+                }
+            });
+            res.json(result);
+        } else {
+            res.status(400).send('no user');
+        }
+    }catch(error){
+        console.error(error);
+        next(error);
+    }
+})
 
 module.exports = router;
