4e3cf15ea66ca66fac0128a88e338623bfd5deb0
shshshee <sehhe22@naver.com>
2021-02-17 00:36:55 +0900

    add photo [Added]

--- routes/page.js
+++ routes/page.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares'); // middlewares.js에서 해당 라우터 불러오기
 const { Post, User, Hashtag } = require('../models'); // post, user 가져오기
 const { Op } = require("sequelize");
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 
 const router = express.Router();
 
@@ -13,9 +16,51 @@ router.use((req,res,next) => {
     next();
 });
 
+try {
+    fs.readdirSync('profiles');
+}catch (error) {
+    console.log('profiles 폴더가 없어 폴더를 생성합니다.');
+    fs.mkdirSync('profiles'); // profiles 폴더 생성
+}
+
+//multer 함수로 미들웨어 생성
+const upload = multer({
+    // 이미지를 서버 디스크에 저장
+    storage: multer.diskStorage({
+        destination(req,file,cb) {
+            cb(null,'profiles/'); // 콜백함수를 통해 전송된 파일 저장 디렉토리 설정
+        },
+        filename(req,file,cb) {
+            const ext = path.extname(file.originalname); // ex photo.png
+            cb(null,path.basename(file.originalname,ext)+Date.now()+ext); // basename: 파일이름 출력
+        }
+    }),
+    limits: {fileSize: 5*1024*1024},
+});
+
 router.get('/profile', isLoggedIn, (req,res) => {
     res.render('profile', {title:'내 정보 - NodeBird'});
 });
+
+//프로필사진 보기
+/*router.post('/photo', isLoggedIn, upload.single('img'), (req,res) => {
+    console.log(req.file);
+    res.json({ url: `/img/${req.file.filename}`}); // 저장된 파일에 대한 정보는 req.file 객체에 담김
+});*/
+
+router.post('/addPhoto',isLoggedIn,upload.single('img'), async(req,res,next)=> {
+    try{
+        const result = await User.update({
+            photo: req.body.url
+        },{
+            where: {id:req.user.id}
+        })
+        res.redirect('/');
+    }catch(error) {
+        console.error(error);
+        next(error);
+    }
+})
 
 router.get('/join', isNotLoggedIn ,(req,res) => {
     res.render('join',{ title: '회원가입 - NodeBird'});
